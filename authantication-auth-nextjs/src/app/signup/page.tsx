975f1b13667a6496e5171966b3d9f9e2b4852b79
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-11-24 01:48:21 +0530

    implement signup functionality with form handling and API integration

--- authantication-auth-nextjs/src/app/signup/page.tsx
+++ authantication-auth-nextjs/src/app/signup/page.tsx
@@ -1,6 +1,45 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import toast, { Toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+
 
 function page() {
+    const router = useRouter();
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        username: "",
+    });
+
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const onSignup=async()=>{
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+
+            console.log(response.data);
+            router.push("/login");
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    useEffect(()=>{
+        if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+            setButtonDisabled(false);
+        }else{
+            setButtonDisabled(true);
+        }
+    }, [user])
+
   return (
     <div>page</div>
   )
